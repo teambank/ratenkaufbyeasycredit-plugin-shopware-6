956c43b8a118b5cbbcf37163c714b65cdddf74f0
Dominik Krebs <dominik.krebs@netzkollektiv.com>
2020-09-10 16:13:03 +0200

    fix: only mark checkbox as required if ratenkauf by easyCredit is selected (first load)

--- src/Resources/app/storefront/src/checkout/checkout.js
+++ src/Resources/app/storefront/src/checkout/checkout.js
@@ -3,15 +3,18 @@ import Plugin from 'src/plugin-system/plugin.class';
 export default class EasyCreditRatenkaufCheckout extends Plugin {
     init() {
 
-        var form = $('#easycredit-payment-form');
-        $('input[type=radio][name=paymentMethodId]').change(function() {
+        var handleEasyCreditRequired = function() {
+            var form = $('#easycredit-payment-form');
             if (form.closest('.payment-method').find($(this)).length > 0) {
                 $('#easycredit-agreement').attr('required','required');
             } else {
                 $('#easycredit-agreement').removeAttr('required');
             }
-        });
-        
+        }
+
+        $('input[type=radio][name=paymentMethodId]:checked').each(handleEasyCreditRequired);
+        $('input[type=radio][name=paymentMethodId]').change(handleEasyCreditRequired);
+
         $('#easycredit-recalculate').click(function(){
             $('#confirmPaymentForm').submit();
             return false;
