657143c6b02b55ef24a5fda95c3dc036aa87ab29
Dominik Krebs <dominik.krebs@netzkollektiv.com>
2021-04-14 10:02:19 +0200

    integrated easycredit checkout widget

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,5 +1,6 @@
 import EasyCreditRatenkaufCheckout from './checkout/checkout';
 import EasyCreditRatenkaufWidget from './widget/widget';
+import './easycredit-checkout';
 
 const PluginManager = window.PluginManager;
 PluginManager.register('EasyCreditRatenkaufCheckout', EasyCreditRatenkaufCheckout, '.is-ctl-checkout.is-act-confirmpage');
