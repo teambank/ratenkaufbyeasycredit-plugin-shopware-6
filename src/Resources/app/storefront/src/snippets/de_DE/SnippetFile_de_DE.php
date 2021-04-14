6df4f3d6a2c066a3d14f0f19737d4937bd3576a9
Dominik Krebs <dominik.krebs@netzkollektiv.com>
2021-04-14 08:56:46 +0200

    changes for sw 6.4

--- src/Resources/app/storefront/src/snippets/de_DE/SnippetFile_de_DE.php
+++ src/Resources/app/storefront/src/snippets/de_DE/SnippetFile_de_DE.php
@@ -18,7 +18,7 @@ class SnippetFile_de_DE implements SnippetFileInterface
 
     public function getPath(): string
     {
-        return __DIR__ . '/easycredit.de-DE.json';
+        return __DIR__ . '/../../../../../snippet/de_DE/easycredit.de-DE.json';
     }
 
     public function getIso(): string
