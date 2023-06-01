fef2b80de68bf385078e9b6a248d0962c5bba2e0
NETZKOLLEKTIV <corporate@netzkollektiv.com>
2023-06-01 11:40:26 +0200

    refactor logger for cross-compatibility reasons

---
+++ src/Util/LoggerConfigurator.php
@@ -0,0 +1,39 @@
+<?php
+namespace Netzkollektiv\EasyCredit\Util;
+
+use Netzkollektiv\EasyCredit\Setting\Exception\SettingsInvalidException;
+use Netzkollektiv\EasyCredit\Setting\Service\SettingsServiceInterface;
+use Psr\Log\LogLevel;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Shopware\Core\PlatformRequest;
+
+class LoggerConfigurator
+{
+    private SettingsServiceInterface $settings;
+
+    protected $requestStack;
+
+    public function __construct(
+        SettingsServiceInterface $settingsService,
+        RequestStack $requestStack
+    ) {
+        $this->settings = $settingsService;
+        $this->requestStack = $requestStack;
+    }
+
+    public function configure($logger): void
+    {
+
+        $request = $this->requestStack->getCurrentRequest();
+        $salesChannelId = null;
+        if ($request && $request->attributes->get(PlatformRequest::ATTRIBUTE_SALES_CHANNEL_ID)) {
+            $salesChannelId = $request->attributes->get(PlatformRequest::ATTRIBUTE_SALES_CHANNEL_ID);
+        }
+
+        $debug = $this->settings->getSettings($salesChannelId)->getDebug();
+        
+        foreach ($logger->getHandlers() as $handler) {
+            $handler->setLevel($debug ? LogLevel::DEBUG : LogLevel::ERROR);
+        }
+    }
+}
