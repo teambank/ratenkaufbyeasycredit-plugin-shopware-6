8ff00973c89a2081c221aecbaa4ccaa44b589a7a
NETZKOLLEKTIV <corporate@netzkollektiv.com>
2023-08-31 10:40:25 +0200

    move js file to assets root, otherwise it got removed by administration build process, see shopware/platform@baf6f1f0c28f25e9b96e3dd6fb0272b24446a7c8

--- esbuild.mjs
+++ esbuild.mjs
@@ -3,7 +3,7 @@ import browserslistToEsbuild from 'browserslist-to-esbuild'
 
 const config = {
   entryPoints: ['src/Resources/app/storefront/src/_main.js'],
-  outfile: 'src/Resources/public/static/js/easycredit.js',
+  outfile: 'src/Resources/public/easycredit.js',
   bundle: true,
   target: browserslistToEsbuild(),
   loader: {
@@ -14,6 +14,6 @@ const config = {
 build(config).catch(() => process.exit(1))
 build({
   ...config, 
-  outfile: 'src/Resources/public/static/js/easycredit.min.js',
+  outfile: 'src/Resources/public/easycredit.min.js',
   minify: true
 }).catch(() => process.exit(1))
