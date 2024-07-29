ca1e332032ec639e7f2e0f79eff3e8a9878ce3ce
NETZKOLLEKTIV <corporate@netzkollektiv.com>
2024-07-29 14:35:44 +0200

    backend changes for bill payment

--- src/Resources/app/administration/src/module/easycredit-payment/component/easycredit-intro/index.js
+++ src/Resources/app/administration/src/module/easycredit-payment/component/easycredit-intro/index.js
@@ -6,8 +6,10 @@ const { Component } = Shopware;
 Component.register('easycredit-intro', {
     template,
 
+    inject: ['EasyCreditRatenkaufApiCredentialsService'],
     data() {
         return {
+            billPaymentActive: false
         };
     },
 
@@ -17,6 +19,45 @@ Component.register('easycredit-intro', {
         }
     },
 
-    methods: {
+    beforeMount () {
+        this.fetchWebshopInfo().then((data) => {
+            this.billPaymentActive = data.billPaymentActive;
+            console.log('billPaymentActive:', data.billPaymentActive);
+        });
     },
+    methods: {
+        getConfigComponent () {
+            var component = this
+            while (component.$parent) {
+                if (typeof component.currentSalesChannelId !== 'undefined') {
+                    return component
+                }
+                component = component.$parent
+            }
+        },
+        getConfig(salesChannelId) {
+            return this.getConfigComponent().actualConfigData[salesChannelId]
+        },
+        getCurrentSalesChannelId() {
+            return this.getConfigComponent().currentSalesChannelId
+        },
+        fetchWebshopInfo () {
+            const salesChannelId = this.getCurrentSalesChannelId();
+            const webshopId = this.getConfig(salesChannelId)['EasyCreditRatenkauf.config.webshopId'] ||
+                this.getConfig(null)['EasyCreditRatenkauf.settings.webshopId'];
+            const url = `https://ratenkauf.easycredit.de/api/payment/v3/webshop/${webshopId}`;
+
+            return fetch(url).then((response) => {
+                if (!response.ok) { 
+                    return Promise.reject(response); 
+                }
+                return response.json();
+            }).then((data) => {
+                // console.log('Fetched data:', data);
+                return data;
+            }).catch((error) => {
+                console.error('Error fetching webshop info:', error);
+            });
+        }
+    }
 });
